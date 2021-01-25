622d980d2200834d4f187db3ab0574ef113733a9
Óscar Grande <oscargrgm@gmail.com>
2021-01-25 18:00:17 +0100

    Created simple notification.

--- app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -17,25 +17,21 @@
 package com.example.android.eggtimernotifications.util
 
 import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.Context
-import android.content.Intent
-import android.graphics.BitmapFactory
 import androidx.core.app.NotificationCompat
-import com.example.android.eggtimernotifications.MainActivity
 import com.example.android.eggtimernotifications.R
-import com.example.android.eggtimernotifications.receiver.SnoozeReceiver
 
 // Notification ID.
-private val NOTIFICATION_ID = 0
-private val REQUEST_CODE = 0
-private val FLAGS = 0
+private const val NOTIFICATION_ID = 0
+private const val REQUEST_CODE = 0
+private const val FLAGS = 0
 
 // TODO: Step 1.1 extension function to send messages (GIVEN)
 /**
  * Builds and delivers the notification.
  *
- * @param context, activity context.
+ * @param messageBody, message body.
+ * @param applicationContext, activity context.
  */
 fun NotificationManager.sendNotification(messageBody: String, applicationContext: Context) {
     // Create the content intent for the notification, which launches
@@ -49,22 +45,29 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
     // TODO: Step 2.2 add snooze action
 
     // TODO: Step 1.2 get an instance of NotificationCompat.Builder
-    // Build the notification
+    //  Build the notification
+    val builder = NotificationCompat.Builder(
+        applicationContext,
+        applicationContext.getString(R.string.egg_notification_channel_id)
+    )
 
     // TODO: Step 1.8 use the new 'breakfast' notification channel
 
     // TODO: Step 1.3 set title, text and icon to builder
+    builder.setSmallIcon(R.drawable.cooked_egg)
+        .setContentTitle(applicationContext.getString(R.string.notification_title))
+        .setContentText(messageBody)
 
     // TODO: Step 1.13 set content intent
 
-        // TODO: Step 2.1 add style to builder
+    // TODO: Step 2.1 add style to builder
 
-        // TODO: Step 2.3 add snooze action
+    // TODO: Step 2.3 add snooze action
 
-        // TODO: Step 2.5 set priority
+    // TODO: Step 2.5 set priority
 
     // TODO: Step 1.4 call notify
-
+    notify(NOTIFICATION_ID, builder.build())
 }
 
 // TODO: Step 1.14 Cancel all notifications
